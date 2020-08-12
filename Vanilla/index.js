d0b3541be055746d20f7e04733808a390a36ed8c
Guy Sudri <guysu@wix.com>
2020-08-12 17:26:29 +0300

    Added delete and mark as completed

--- Vanilla/index.js
+++ Vanilla/index.js
@@ -7,11 +7,46 @@ function SingleTask(id, title) {
     this.title = title;
 }
 
-function handleAddTask() {
-    console.log(document.getElementById("newTask").value);
-    var test = document.createElement("p");
-    var test1 = document.createTextNode(document.getElementById("newTask").value);
-    test.appendChild(test1);
-    var theDiv = document.getElementById("allTasks");
-    theDiv.appendChild(test);
+
+
+document.querySelector('.add_task_btn').addEventListener('click', function() {
+    let html = '<div class="single_task" id="task_%id%"><input type="checkbox" id="task_status_%id%"/> <span class="task_title_%id%">%title%</span> \
+    <button class="edit_btn" id="edit_%id%">Edit</button> <button class="delete_btn" id="delete_%id%">Delete</button></div>'
+    html = html.replace(/%id%/g, id);
+    document.querySelector('.unfinished_tasks').insertAdjacentHTML('beforeend', html);
+    id++;
+})
+
+
+document.querySelector('.tasks_container').addEventListener('click', function(event) {
+    let eventID = event.target.id;
+    // For deleting task
+    if (eventID.startsWith('delete')) {
+        handleDelete(event);
+    }
+    // For checking finished or unfinished task
+    else if (eventID.startsWith('task_status')) {
+        handleFinished(event); 
+    }
+})
+
+
+function handleFinished(event) {
+    let eventID = event.target.id;
+    let taskTitleID = eventID.split('_')[2];
+
+    if (document.querySelector('#' + eventID).checked) { //Moves task to bottom of finished
+        document.querySelector('.finished_tasks').appendChild(event.target.parentNode);
+    }
+    else {
+        document.querySelector('.unfinished_tasks').appendChild(event.target.parentNode);
+    }
+
+    document.querySelector('.task_title_' + taskTitleID).classList.toggle('finished_task'); //Adds line through if finished
 }
+
+function handleDelete(event) {
+    let parentID = event.target.parentNode.id;
+    document.querySelector('#' + parentID).remove();
+}
+
