e9b26acb3f0aebb0ed5df711c3bb94b8571853f3
Guy Sudri <guysu@wix.com>
2020-08-26 15:51:40 +0300

    test

--- Server/client/src/index.js
+++ Server/client/src/index.js
@@ -3,6 +3,8 @@ import "./style.scss";
 import { getAllTodosFromServer } from "./server-api";
 import { $ } from "./utils";
 
+//TESTING!
+
 window.onload = () => {
     $("body").style.display = "block";
 };
