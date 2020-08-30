18fc86023b17b101ceb70ddb09d1948a7c3f99e8
Guy Sudri <guysu@wix.com>
2020-08-30 16:44:01 +0300

    todo | Added get and post on redis

--- Server/server/index.js
+++ Server/server/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const path = require("path");
 const uuid = require("uuid");
 const cookieParser = require("cookie-parser");
+const { type } = require("os");
 const app = express();
+const client = require("redis").createClient(process.env.REDIS_URL);
+client.on("connect", function () {
+    console.log("Successfully connected");
+});
+// client.flushall();
 
 const PORT = process.env.PORT || 80;
 
@@ -14,20 +20,36 @@ const getUserID = (req) => req.cookies.id;
 let todos = new Map();
 
 app.get("/todos", (req, res) => {
-    let userID = getUserID(req);
+    /* let userID = getUserID(req);
     if (!userID) {
         userID = uuid.v4();
         todos.set(userID, []);
         res.cookie("id", userID, { httpOnly: true });
     }
-    res.send(todos.get(userID));
+    res.send(todos.get(userID)); */
+    let userID = getUserID(req);
+    if (!userID) {
+        userID = uuid.v4();
+        const emptyArray = JSON.stringify([]);
+        client.hmset(userID, "todos", emptyArray);
+        res.cookie("id", userID, { httpOnly: true });
+    }
+    client.hmget(userID, "todos", (err, obj) => {
+        console.log(obj);
+        res.send(JSON.parse(obj));
+    });
 });
 
 app.post("/todos", (req, res) => {
     let userID = getUserID(req);
     const taskID = uuid.v4();
     const newTask = { id: taskID, ...req.body };
-    todos.get(userID).push(newTask);
+    // todos.get(userID).push(newTask);
+    client.hmget(userID, "todos", (err, obj) => {
+        const newArray = JSON.parse(obj);
+        newArray.push(newTask);
+        client.hmset(userID, "todos", JSON.stringify(newArray));
+    });
     res.send(newTask);
 });
 
