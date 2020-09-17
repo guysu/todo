8a627957f9876f500fae68c8ecbc2951f07bc7c4
Guy Sudri <guysu@wix.com>
2020-09-17 13:10:09 +0300

    todo | Attempted to test with puppeteer

--- React/src/components/App/App.test.tsx
+++ React/src/components/App/App.test.tsx
@@ -7,6 +7,7 @@ import Header from "../Header/Header";
 import TodoList from "../TodoList/TodoList";
 import axios from "axios";
 import { act } from "react-dom/test-utils";
+import puppeteer from "puppeteer";
 
 configure({ adapter: new Adapter() });
 
@@ -17,7 +18,7 @@ describe("Testing <App />", () => {
         jest.spyOn(axios, "get").mockResolvedValue({
             status: 200,
             statusText: "OK",
-            data: [{ id: "", title: "", checked: false }],
+            data: [{ id: "Test", title: "", checked: false }],
         });
         await act(async () => {
             wrapper = await mount(<App />);
@@ -31,4 +32,31 @@ describe("Testing <App />", () => {
     it("Should render one <TodoList />", async () => {
         expect(wrapper.find(TodoList).exists()).toEqual(true);
     });
+
+    it("Should render one task after adding", async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("http://localhost:80");
+        await page.click(".add-task-input");
+        await page.type(".add-task-input", "Testing");
+        await page.click(".add-task-btn");
+        await page.waitForSelector(`[data-hook="task-title"]`);
+        const singleTaskText = await page.$eval(`[data-hook="task-title"]`, (res) => res.textContent);
+        expect(singleTaskText).toEqual("Testing");
+
+        browser.close();
+    });
+
+    // it("Should render one task from server", async () => {
+    //     jest.spyOn(axios, "get").mockResolvedValue({
+    //         status: 200,
+    //         statusText: "OK",
+    //         data: [{ id: "123", title: "Test", checked: false }],
+    //     });
+    //     const browser = await puppeteer.launch({headless:false});
+    //     const page = await browser.newPage();
+    //     await page.goto("file:///Users/guysu/Desktop/react-apps/todo/React/src/index.ejs");
+
+    //     browser.close();
+    // })
 });
